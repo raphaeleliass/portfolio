9346cce9826f87dd30c96cb7ff23afa2846eab6a
Raphael Elias <raphaeleliass@outlook.com>
2025-07-28 12:28:47 -0300

    feat: add index types and constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const appBaseUrl = process.env.NEXT_PUBLIC_APP_BASE_URL;
+export const appApiKey = process.env.APP_API_KEY;
+export const appUserId = process.env.APP_USER_ID;
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface ProjectTypes {
+  id: string;
+  title: string;
+  description: string;
+  techs: [];
+  url: null;
+  repo_url: string;
+  image_url: [];
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+}
+[];
