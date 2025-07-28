8b3f18b8c02a2017341650b28d8afaa9c39e5252
Raphael Elias <raphaeleliass@outlook.com>
2025-07-28 12:22:23 -0300

    feat: add option to send more than 1mb forms

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
