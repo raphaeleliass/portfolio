e18dd6f2cbbe748727180f9b3e8f52623929db49
Raphael Elias <raphaeleliass@outlook.com>
2025-08-12 10:39:55 -0300

    refactor: add a reusable css class to section element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 import Experience from "@/components/layout/home/experience";
 import Projects from "@/components/layout/home/projects";
-import Navbar from "@/components/ui/navbar";
 import { fetchProjects } from "@/lib/fetchProjects";
 
 export default async function Home() {
   const data = await fetchProjects();
 
   return (
-    <>
-      <Navbar />
+    <main className="section-view">
+      <Experience className="border-muted-foreground max-sm:border-b md:border-r" />
 
-      <main className="mx-auto flex h-[89dvh] flex-col md:flex-row lg:max-w-7xl">
-        <Experience className="border-muted-foreground max-sm:border-b md:border-r" />
-
-        <Projects projects={data} />
-      </main>
-    </>
+      <Projects projects={data} />
+    </main>
   );
 }
