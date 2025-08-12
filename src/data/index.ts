a22dd0dec9f14abe137dc6558b82f64f046d3534
Raphael Elias <raphaeleliass@outlook.com>
2025-08-12 01:35:18 -0300

    feat: add a index file data

---
+++ src/data/index.ts
@@ -0,0 +1,48 @@
+// user experience
+
+export const experiences = [
+  {
+    title: "Linkasaur",
+    date: "Ago 2025",
+    role: "Desenvolvedor Full-stack",
+    description:
+      "Desenvolvimento de monorepo com Turborepo, Next.js e Hono para um gerenciador de links voltado a usuários gratuitos e premium. Banco de dados PostgreSQL e autenticação completa. Arquitetura projetada para escalar, com estrutura robusta e otimizada para alta performance.",
+    working: true,
+  },
+  {
+    title: "Linkgrid API",
+    date: "Mai 2025",
+    role: "Desenvolvedor Back-end",
+    description:
+      "Desenvolvimento de API escalável com Express, aplicando MVC, SOLID e validações com Zod. Implementação de segurança com CORS, Rate Limit e Helmet. Integração com PostgreSQL via Prisma ORM. Publicação com CI/CD em ambiente de produção.",
+    working: false,
+  },
+  {
+    title: "B-Side",
+    date: "Jun 2025",
+    role: "Desenvolvedor Full-stack",
+    description:
+      "Projeto em Next.js com API Routes, Prisma e MongoDB. Integração de autenticação OAuth com Spotify e consumo da API oficial para gerar dinamicamente um arquivo PNG simulando uma capa de vinil com os artistas e álbuns mais ouvidos nos últimos 30 dias.",
+    working: true,
+  },
+];
+
+// user social-media
+export const socialMedia = [
+  {
+    title: "Linkedin",
+    href: "https://linkedin.com/in/raphaeleliass",
+  },
+  {
+    title: "Github",
+    href: "https://github.com/raphaeleliass",
+  },
+  {
+    title: "Instagram",
+    href: "https://instagram.com/raphaeleliass",
+  },
+  {
+    title: "Email",
+    href: "mailto:raphaeleliass@outlook.com",
+  },
+];
