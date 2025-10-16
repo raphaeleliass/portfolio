8c01a396a847844387a6ec3a74bb7ae77b4cce5e
Raphael Elias <raphaeleliass@outlook.com>
2025-10-16 20:09:15 -0300

    feat(web): add components configuration, update Next.js config, and enhance package dependencies

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		qualities: [50, 75, 100],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "picsum.photos",
+			},
+		],
+	},
 };
 
 export default nextConfig;
