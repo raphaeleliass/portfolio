76c27ede78085de778853b57ccbf20f032a22c63
Raphael Elias <raphaeleliass@outlook.com>
2025-10-16 20:08:27 -0300

    feat(data): add projects data and project types interface

---
+++ apps/web/src/types/index.ts
@@ -0,0 +1,10 @@
+export interface ProjectTypes {
+	id: number;
+	title: string;
+	description: string;
+	status: "published" | "draft" | "archived";
+	createdAt: string;
+	updatedAt: string;
+	imageUrl: string;
+	technologies: { id: number; tech: string }[];
+}
