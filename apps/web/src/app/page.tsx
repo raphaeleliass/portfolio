67da00dece4715e4ccc7efafd027a9de25690aaa
Raphael Elias <raphaeleliass@outlook.com>
2025-10-16 20:08:15 -0300

    feat(web): enhance Home component with Editor layout and project management features

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,103 +1,181 @@
+"use client";
+import { Folder, Markdown } from "@react-symbols/icons";
 import Image from "next/image";
+import { useEffect } from "react";
+import {
+	Editor,
+	EditorContent,
+	EditorFloatingMenu,
+	EditorHeader,
+	EditorSidebar,
+	EditorWrapper,
+} from "@/components/layout/editor";
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { projectsData } from "@/data";
+import { useProjectStore } from "@/store/useProjectsStore";
+import { useSectionStore } from "@/store/useSectionStore";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	const { projects, setProjects, setProject, project } = useProjectStore();
+	const { section, setSection } = useSectionStore();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	//biome-ignore lint: not necessary add dependencies at array
+	useEffect(() => {
+		setProjects(JSON.parse(JSON.stringify(projectsData)));
+		setProject(JSON.parse(JSON.stringify(projectsData[0])));
+	}, []);
+
+	function formatDate(date: string) {
+		const newDate = new Date(date).toLocaleString("pt-br", {
+			month: "long",
+			year: "numeric",
+		});
+
+		return newDate;
+	}
+
+	return (
+		<main className="h-dvh place-content-center justify-items-center bg-gradient-to-tr from-green-500 via-purple-500 to-indigo-500 p-2 md:p-6">
+			<Editor>
+				<EditorHeader>
+					<DropdownMenu>
+						<DropdownMenuTrigger className="mx-auto w-1/3 rounded-full bg-muted-foreground/40 py-1">
+							<p className="text-muted-foreground/90 text-sm">{section}</p>
+						</DropdownMenuTrigger>
+
+						<DropdownMenuContent align="center" className="w-56">
+							<DropdownMenuGroup>
+								<DropdownMenuItem onClick={() => setSection("Home")}>
+									Home
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setSection("Sobre")}>
+									Sobre
+								</DropdownMenuItem>
+							</DropdownMenuGroup>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</EditorHeader>
+
+				<EditorWrapper className="">
+					<EditorSidebar className="">
+						<Accordion type="multiple">
+							<AccordionItem value="projects">
+								<AccordionTrigger className="mt-4 flex cursor-pointer flex-row items-center py-0">
+									<span className="flex flex-row items-center gap-2 py-2 text-sm">
+										<Folder className="size-5" />
+										Projetos
+									</span>
+								</AccordionTrigger>
+
+								<AccordionContent className="flex flex-col justify-between bg-muted-foreground/5 pb-0">
+									{projects.map((i) => (
+										<Button
+											key={i.id}
+											variant={"ghost"}
+											type="button"
+											className="z-50 w-full cursor-pointer justify-start rounded-none text-muted-foreground text-xs data-[active=true]:bg-muted-foreground/20"
+											data-active={project?.title === i.title}
+											onClick={() => setProject(i)}
+										>
+											<Markdown className="size-5" />
+											{i.title.replace(/[ -]/g, "")}.md
+										</Button>
+									))}
+								</AccordionContent>
+							</AccordionItem>
+						</Accordion>
+					</EditorSidebar>
+
+					<EditorContent>
+						<EditorFloatingMenu />
+						{project && (
+							<div className="mx-auto flex w-full flex-col gap-12 max-sm:w-xs md:w-2xl">
+								<h2 className="font-bold text-4xl max-sm:text-2xl">
+									<span className="text-muted-foreground/50">#</span>{" "}
+									{project?.title}
+								</h2>
+								<p className="text-balance text-muted-foreground max-sm:text-sm">
+									{project?.description}
+								</p>
+								<div className="flex flex-col items-start justify-start">
+									<Image
+										src={project.imageUrl}
+										alt={`${project.title} image`}
+										width={1000}
+										height={1000}
+										quality={100}
+										priority
+										className="aspect-video h-auto w-full"
+									/>
+									<p className="mt-2 text-muted-foreground text-xs">
+										{" "}
+										Projeto criado em {formatDate(project.createdAt)}
+									</p>
+								</div>
+
+								<div className="flex w-fit flex-col gap-4 text-sm">
+									<a
+										href={project.imageUrl}
+										target="_blank"
+										rel="noreferrer noopener"
+									>
+										<p className="text-muted-foreground">
+											[
+											<span className="font-semibold text-blue-500 underline underline-offset-2">
+												confira o site
+											</span>
+											]
+										</p>
+									</a>
+									<a
+										href={project.imageUrl}
+										target="_blank"
+										rel="noreferrer noopener"
+									>
+										<p className="text-muted-foreground">
+											[
+											<span className="font-semibold text-blue-500 underline underline-offset-2">
+												confira o repositório
+											</span>
+											]
+										</p>
+									</a>
+								</div>
+
+								<div>
+									<h2 className="inline-flex gap-2 text-muted-foreground/50">
+										**{" "}
+										<span className="font-bold text-foreground">
+											Tecnologias
+										</span>{" "}
+										**
+									</h2>
+									<ul className="list-inside list-disc pl-4 text-muted-foreground">
+										{project.technologies.map(({ tech }) => (
+											<li key={tech} className="text-sm">
+												{tech}
+											</li>
+										))}
+									</ul>
+								</div>
+							</div>
+						)}
+					</EditorContent>
+				</EditorWrapper>
+			</Editor>
+		</main>
+	);
 }
